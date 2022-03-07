69e1ebc1300d0d3d91c2396095aa4d43e5c90d64
G9000 <leo.caesar@live.com>
2022-03-07 18:55:49 +0800

    oops wrong place

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,43 @@ import "../styles/resets.css";
 import "../styles/globals.css";
 import NextApp from "next/app";
 import { Client } from "../prismic-configuration";
+import { useEffect } from "react";
+import * as Fathom from "fathom-client";
+import { useRouter } from "next/router";
 
-export default class MyApp extends NextApp {
-  static async getInitialProps(appCtx) {
-    const client = Client();
-    const menu = (await client.getSingle("menu")) || {};
-    return {
-      props: {
-        menu: menu,
-      },
+const MyApp = (props) => {
+  const router = useRouter();
+  useEffect(() => {
+    Fathom.load("RJUTYVQW", {
+      includedDomains: ["www.juliocaesar.co"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
     };
-  }
+  }, []);
+  const Component = props.Component;
+  const pageProps = props.pageProps;
+  const menu = props.props;
+  return <Component {...pageProps} menu={menu.menu} />;
+};
 
-  render() {
-    const { Component, pageProps, props } = this.props;
-    return <Component {...pageProps} menu={props.menu} />;
-  }
-}
+MyApp.getInitialProps = async (appCtx) => {
+  console.log("menu");
+  const client = Client();
+  const menu = (await client.getSingle("menu")) || {};
+  return {
+    props: {
+      menu: menu,
+    },
+  };
+};
+
+export default MyApp;
